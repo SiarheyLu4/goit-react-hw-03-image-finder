80412826349519be5977dddc4fe32b764e28b16c
Siarhei Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-08-29 13:45:53 +0300

    add styled
    add styled for ImageGallery and ImageGalleryItem

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 
 export const ImageGalleryItem = ({ id, webformatURL, tags }) => {
   return (
-    <li key={id}>
-      <img src={webformatURL} alt={tags} />
-    </li>
+    <Li key={id}>
+      <Image src={webformatURL} alt={tags} />
+    </Li>
   )
 }
+
+const Li = styled.li`
+  border-radius: 2px;
+  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
+`
+
+const Image = styled.img`
+  width: 100%;
+  height: 260px;
+  object-fit: cover;
+  transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
+    &:hover {
+  transform: scale(1.03);
+  cursor: zoom-in;
+}
+`
